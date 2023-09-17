73c77976c1662ba7c4a2d9e95b5adb1ee61fdd94
TanmayS44 <94474384+TanmayS44@users.noreply.github.com>
2023-09-17 21:12:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+let gridSize = 20;
+
+let penColour = "yellow"
+
+const gridContainer = document.querySelector(".grid-container");
+gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+function createGrid(sizeOfGrid) {
+    while(sizeOfGrid > 0 && sizeOfGrid < 10000) {
+        let gridBox = document.createElement("div");
+        gridBox.classList.add("grid-item");
+        gridBox.classList.add("btn");
+        gridContainer.appendChild(gridBox);
+        sizeOfGrid--;
+    }
+}
+
+createGrid(gridSize*gridSize);
+
+const gridBoxes = document.querySelectorAll(".btn");
+gridBoxes.forEach(function (currentValue) {
+    currentValue.addEventListener("mouseover", () => {
+        currentValue.style["background-color"] = penColour;
+    });
+})
+
+const colourSelectorButton = document.getElementById("colour-selector")
+colourSelectorButton.addEventListener("click", () => {
+    penColour = prompt("Enter pen colour (regular single word basic colours only):");
+});
+
+const resetButton = document.getElementById("reset-button");
+resetButton.addEventListener("click", () => {
+    gridBoxes.forEach(function (currentValue) {
+        currentValue.style["background-color"] = "black";
+    });
+});
+
+const gridChangeButton = document.getElementById("change-grid-size");
+gridChangeButton.addEventListener("click", () => {
+    gridSize = prompt("What size should the grid be? (X * X units)");
+
+    // Clear the existing grid items
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+    createGrid(gridSize * gridSize);
+
+    // Reattach event listeners to the new grid items
+    const gridBoxes = document.querySelectorAll(".btn");
+    gridBoxes.forEach(function (currentValue) {
+        currentValue.addEventListener("mouseover", () => {
+            currentValue.style["background-color"] = penColour;
+        });
+    });
+
+    // Reattach event listener to the "Reset" button
+    resetButton.addEventListener("click", () => {
+        gridBoxes.forEach(function (currentValue) {
+            currentValue.style["background-color"] = "black";
+        });
+    });
+});
