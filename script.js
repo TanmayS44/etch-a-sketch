a6657138d6a9f60a8e918f2b1eb50fe6f4c00cc2
tanmay <94474384+TanmayS44@users.noreply.github.com>
2024-01-04 13:20:37 +0530

    Remove redundant code

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
 gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 
 function createGrid(sizeOfGrid) {
-    while(sizeOfGrid > 0 && sizeOfGrid < 10000) {
+    while(sizeOfGrid > 0 && sizeOfGrid <= 10000) {
         let gridBox = document.createElement("div");
         gridBox.classList.add("grid-item");
         gridBox.classList.add("btn");
@@ -18,16 +18,32 @@ function createGrid(sizeOfGrid) {
 
 createGrid(gridSize*gridSize);
 
+// check for click and drag
+let mouseDown = false;
+
+document.addEventListener("mousedown", () => {
+    mouseDown = true;
+});
+
+document.addEventListener("mouseup", () => {
+    mouseDown = false;
+});
+
 const gridBoxes = document.querySelectorAll(".btn");
-gridBoxes.forEach(function (currentValue) {
+
+gridBoxes.forEach(setPenColour);
+
+function setPenColour (currentValue) {
     currentValue.addEventListener("mouseover", () => {
-        currentValue.style["background-color"] = penColour;
+        if (mouseDown) {
+            currentValue.style["background-color"] = penColour;
+        }
     });
-})
+}
 
 const colourSelectorButton = document.getElementById("colour-selector")
 colourSelectorButton.addEventListener("click", () => {
-    penColour = prompt("Enter pen colour (regular single word basic colours only):");
+    penColour = prompt("Enter pen colour (single word basic colours only):");
 });
 
 const resetButton = document.getElementById("reset-button");
@@ -37,27 +53,24 @@ resetButton.addEventListener("click", () => {
     });
 });
 
+// -----------------------------------------
 const gridChangeButton = document.getElementById("change-grid-size");
 gridChangeButton.addEventListener("click", () => {
-    gridSize = prompt("What size should the grid be? (X * X units)");
+    gridSize = prompt("Enter grid side X (Grid will be X * X units)");
 
-    // Clear the existing grid items
+    // Clear the existing grid items, then create new grid
     while (gridContainer.firstChild) {
         gridContainer.removeChild(gridContainer.firstChild);
     }
-
     gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 
     createGrid(gridSize * gridSize);
 
     // Reattach event listeners to the new grid items
+
     const gridBoxes = document.querySelectorAll(".btn");
-    gridBoxes.forEach(function (currentValue) {
-        currentValue.addEventListener("mouseover", () => {
-            currentValue.style["background-color"] = penColour;
-        });
-    });
+    gridBoxes.forEach(setPenColour);
 
     // Reattach event listener to the "Reset" button
     resetButton.addEventListener("click", () => {
